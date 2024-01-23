3d34987bfe98a46a4e0ba97d8de566092f3779f4
KrokoNinja <lenni.pfundtner@gmail.com>
2024-01-23 13:26:39 +0100

    change Home content and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Home from "@/components/pages/Home";
-import Nav from "@/components/ui/Nav";
 
 export default function RootPage() {
   return (
-    <main className="flex flex-col gap-12 px-12 py-6 md:px-24 lg:px-24 lg:py-12">
+    <main className="h-full flex flex-col justify-center gap-12 px-12 py-6 md:px-24 lg:px-24 lg:py-12">
       <Home />
     </main>
   );
--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,14 +1,17 @@
 import { Button } from "../ui/button";
 
 export default function Home() {
-    return (
-        <section className="flex flex-col justify-center text-center gap-6">
-            <h1 className="text-4xl font-bold">Learning shadcn</h1>
-            <p className="text-2xl">Improving my styling with shadcn & themes</p>
-            <div className="flex flex-row gap-2 justify-center">
-            <Button variant={"secondary"}>Learn with me</Button>
-            <Button>Learn for yourself</Button>
-            </div>
-        </section>
-    );
+  return (
+    <section className="flex flex-col justify-center text-center gap-6">
+      <h1 className="text-4xl font-bold">Meetingroom Planer</h1>
+      <p className="text-2xl">
+        Plan meetings accross different rooms, without struggeling with
+        overlapping or multiple meetings in one room at the same time.
+      </p>
+      <div className="flex flex-row gap-2 justify-center">
+        <Button variant={"secondary"}>View Walkthrough</Button>
+        <Button>Try it yourself</Button>
+      </div>
+    </section>
+  );
 }
